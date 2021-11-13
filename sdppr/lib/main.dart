adefafd4f7700aa466625fec299e3f3851577573
Darshan <73795559+darshannere@users.noreply.github.com>
2021-11-14 00:37:58 +0530

    .

--- sdppr/lib/main.dart
+++ sdppr/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'login/home.dart';
+import 'dart:ui';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +13,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      routes: {
-        '/': (context) => const MyHomePage(),
-      },
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginScreen(),
     );
   }
 }
